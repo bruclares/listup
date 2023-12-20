dc025341d1676298b802ff07c1cbdf5dec1cfeca
bruclares <clares.bruna@gmail.com>
2023-12-19 22:13:59 -0300

    continuação controller_task

--- app/controllers/task_controller.py
+++ app/controllers/task_controller.py
@@ -1,11 +1,13 @@
-from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import (
+    Blueprint, flash, redirect, render_template, request, session, url_for
+)
+
 from ..database.db import get_db
 from .auth_controller import login_required
 
 bp = Blueprint('task', __name__, url_prefix='/tarefas')
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/')
 @login_required
 def list():
     user_id = session.get('user_id')
@@ -16,7 +18,9 @@ def list():
     if search:
         query += ' and description ilike %s'
         parameters += ('%' + (search if search else '') + '%',)
-      
+    
+    query += ' ORDER BY conclusion_date DESC, id'
+
     with get_db() as conn:
         with conn.cursor() as curs:
             curs.execute(
@@ -36,7 +40,7 @@ def list():
 def create():
     if request.method == 'POST':
         user_id = session.get('user_id')
-        description = request.form['description'].capitalize()
+        description = request.form['description']
         category_id = request.form['category_id']
 
         error = None
@@ -59,3 +63,49 @@ def create():
 
     return render_template('tarefas/create_task.html')
 
+@bp.route('<id>/alterar', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    if request.method == 'POST':
+        description = request.form['description']
+        category_id = request.form['category_id']
+
+        error = None
+
+        if not description:
+            error = 'A tarefa é obrigatória.'
+
+        if error is None:
+            with get_db() as conn:
+                with conn.cursor() as curs:
+                    curs.execute(
+                        '''UPDATE tasks SET description = %s, category_id = %s
+                        WHERE id = %s''',
+                        (description, category_id, id))
+            
+            return redirect(url_for('task.list'))
+        
+        flash(error)
+
+    with get_db() as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                'SELECT * FROM tasks WHERE id = %s',
+                # parametro da função será o valor
+                (id,)
+            )
+            task = curs.fetchone()
+        
+    return render_template('tarefas/update_task.html', task=task)
+
+@bp.route('<id>/conclusao')
+@login_required
+def conclusion(id):
+    with get_db() as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                '''UPDATE tasks SET conclusion_date = CURRENT_TIMESTAMP
+                WHERE id = %s''',
+                (id,))
+    return redirect(url_for('task.list'))
+        
